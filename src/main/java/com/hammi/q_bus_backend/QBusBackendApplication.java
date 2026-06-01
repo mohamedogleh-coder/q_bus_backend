974d360dcb8b1b5db22a2087b5606cef4a1e4d2b
Mohamed Ogleh <mohamedogleh@Mohameds-MacBook-Air.local>
2026-06-01 11:19:56 +0300

    first init alxm.

---
+++ src/main/java/com/hammi/q_bus_backend/QBusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hammi.q_bus_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QBusBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QBusBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hammi/q_bus_backend/QBusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hammi.q_bus_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QBusBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
